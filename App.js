af8d2cc9f151790270f24c73cc1da45b6aead2e4
Priyanshu Pramanik <150166083+pryaaansu@users.noreply.github.com>
2025-04-26 13:13:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import XyloSecurityScanner from './components/XyloSecurityScanner';
+
+function App() {
+  return (
+    <div className="App">
+      <XyloSecurityScanner />
+    </div>
+  );
+}
+
+export default App;
